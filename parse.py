408b3d406f900e44fec8b41fcd5ca9ff024c6da2
Armin Samii <armin.samii@gmail.com>
2021-03-14 11:58:33 -0400

    include special vs primary vs general in results

--- parse.py
+++ parse.py
@@ -1,4 +1,3 @@
-import pprint
 import csv
 import os
 import re
@@ -8,12 +7,17 @@ class PrecinctResult:
     candidatesToVotes:dict
 
 class OneElection:
+    ballotName:str
     electionName:str
     office:str
     district:str
     maybeYear:int
+    isSingleWinner:bool
+    isSpecial:bool
+    isPrimary:bool
 
-    def __init__(self, oneRow):
+    def __init__(self, ballotName, oneRow):
+        self.ballotName = ballotName
         self.electionName = oneRow.electionName
         self.office = oneRow.office
         self.district = oneRow.district
@@ -23,11 +27,33 @@ class OneElection:
         except ValueError:
             self.maybeYear = None
 
+        self.isSingleWinner = self.is_single_winner(self.office)
+        self.isPrimary = 'Primary' in ballotName
+        self.isSpecial = 'Special' in ballotName
+
+    @classmethod
+    def is_single_winner(self, office):
+        known_single_winner_substrings = (
+            "Representative in",
+            "President of the United States",
+            "Attorney General",
+            "State Treasurer",
+            "Representative in",
+            "Senator in",
+            "United States Senator",
+            "Auditor General",
+            "Governor",
+            "Lieutenant Governor")
+        return any(substring in office for substring in known_single_winner_substrings)
+
+    def _fields(self):
+        return (self.ballotName, self.electionName, self.office, self.district)
+
     def __hash__(self):
-        return hash((self.electionName, self.office, self.district))
+        return hash(self._fields())
 
     def __eq__(self, other):
-        return (self.electionName, self.office, self.district) == (other.electionName, other.office, other.district)
+        return self._fields() == other._fields()
 
 class OneRow:
     electionName:str
@@ -61,7 +87,7 @@ def parseFile(filepath):
     return d
 
 def parseAllFiles(maindir):
-    dataPerElection = []
+    dataPerBallotName = {}
     for filename in os.listdir(maindir):
         if not filename.endswith('.CSV'):
             continue
@@ -69,15 +95,15 @@ def parseAllFiles(maindir):
 
         print("Parsing ", filepath)
         result = parseFile(filepath)
-        dataPerElection.append(result)
+        dataPerBallotName[filename] = result
 
-    return dataPerElection
+    return dataPerBallotName
 
-def getPrecinctResultsPerElection(data):
+def getPrecinctResultsPerElection(dataPerBallotName):
     resultsPerElection = {}
-    for election in data:
+    for ballotName, election in dataPerBallotName.items():
         for row in election:
-            election = OneElection(row)
+            election = OneElection(ballotName, row)
 
             # Add election to dataset if we haven't seen it before
             if election not in resultsPerElection:
@@ -94,20 +120,39 @@ def getPrecinctResultsPerElection(data):
 
 def main():
     maindir = 'downloads'
-    data = parseAllFiles(maindir)
-    resultsPerElection = getPrecinctResultsPerElection(data)
-    pprint.pprint(resultsPerElection)
+    dataPerBallotName = parseAllFiles(maindir)
+    resultsPerElection = getPrecinctResultsPerElection(dataPerBallotName)
 
     with open('results.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerow(['year', 'electionname', 'district', 'office', 'num candidates', 'percent winner won with', 'all running candidates'])
+        csvwriter.writerow(['year',
+                            'ballotname',
+                            'electionname',
+                            'district',
+                            'office',
+                            'special election?',
+                            'primary?',
+                            'single-winner?',
+                            'num candidates',
+                            'percent winner won with',
+                            'all running candidates'])
         for election, votes in resultsPerElection.items():
             totalVotes = sum(votes.values())
             maxVotes = max(votes.values())
             if totalVotes == 0:
                 continue
             
-            csvwriter.writerow([election.maybeYear, election.electionName, election.district, election.office, len(votes), maxVotes/totalVotes, '/'.join(votes.keys())])
+            csvwriter.writerow([election.maybeYear,
+                                election.ballotName,
+                                election.electionName,
+                                election.district,
+                                election.office,
+                                election.isSpecial,
+                                election.isPrimary,
+                                election.isSingleWinner,
+                                len(votes),
+                                maxVotes/totalVotes,
+                                '/'.join(votes.keys())])
 
 
 if __name__ == "__main__":
