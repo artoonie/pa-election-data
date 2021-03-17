666ab309f4502271f2f960a27474f4c039792c39
Armin Samii <armin.samii@gmail.com>
2021-03-17 18:17:33 -0400

    Fix 2019 special, update year column

--- parse.py
+++ parse.py
@@ -11,7 +11,8 @@ class OneElection:
     electionName:str
     office:str
     district:str
-    maybeYear:int
+    party:str # only relevant if isPrimary
+    year:int
     isSingleWinner:bool
     isSpecial:bool
     isPrimary:bool
@@ -22,14 +23,13 @@ class OneElection:
         self.office = oneRow.office
         self.district = oneRow.district
 
-        try:
-            self.maybeYear = int(self.electionName[0:4])
-        except ValueError:
-            self.maybeYear = None
+        self.year = int(self.ballotName[0:4])
 
         self.isSingleWinner = self.is_single_winner(self.office)
         self.isPrimary = 'Primary' in ballotName
         self.isSpecial = 'Special' in ballotName
+
+        self.party = oneRow.party if self.isPrimary else None
 
     @classmethod
     def is_single_winner(self, office):
@@ -47,6 +47,9 @@ class OneElection:
         return any(substring in office for substring in known_single_winner_substrings)
 
     def _fields(self):
+        if self.isPrimary:
+            return (self.ballotName, self.electionName, self.office, self.district, self.party)
+
         return (self.ballotName, self.electionName, self.office, self.district)
 
     def __hash__(self):
@@ -59,6 +62,7 @@ class OneRow:
     electionName:str
     county:str
     office:str
+    party:str
     district:str
     candidateName:str
     numVotes:int
@@ -69,6 +73,7 @@ class OneRow:
         self.office = rawRowData['Office Name']
         self.district = rawRowData['District Name']
         self.candidateName = rawRowData['Candidate Name']
+        self.party = rawRowData['Party Name']
         votes = rawRowData['Votes']
         if votes == '':
             votes = '0'
@@ -130,29 +135,36 @@ def main():
                             'electionname',
                             'district',
                             'office',
+                            'party (if primary)',
                             'special election?',
                             'primary?',
                             'single-winner?',
                             'num candidates',
                             'percent winner won with',
-                            'all running candidates'])
+                            *['candidate %d'%d for d in range(1,11)]])
         for election, votes in resultsPerElection.items():
             totalVotes = sum(votes.values())
             maxVotes = max(votes.values())
             if totalVotes == 0:
                 continue
+
+            eachVotePct = [v/totalVotes for v in votes.values()]
+            namesAndVotes = list(zip(eachVotePct, votes.keys()))
+            namesAndVotes.sort(reverse=True)
+            namesAndVotes = ["%s (%02.1f%%)" % (nv[1], 100*nv[0]) for nv in namesAndVotes]
             
-            csvwriter.writerow([election.maybeYear,
+            csvwriter.writerow([election.year,
                                 election.ballotName,
                                 election.electionName,
                                 election.district,
                                 election.office,
+                                election.party,
                                 election.isSpecial,
                                 election.isPrimary,
                                 election.isSingleWinner,
                                 len(votes),
                                 maxVotes/totalVotes,
-                                '/'.join(votes.keys())])
+                                *namesAndVotes])
 
 
 if __name__ == "__main__":
